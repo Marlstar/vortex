ba143f949ca34c87d797564ebf2c3d4c47edd531
Marley Reeves <marley@marlstar.com>
2025-05-14 14:19:49 +1000

    feat: base argument and subcommand implementation

---
+++ src/args.rs
@@ -0,0 +1,37 @@
+use clap::{Parser, Subcommand};
+use std::{path::PathBuf, sync::LazyLock};
+
+pub static ARGS: LazyLock<Args> = LazyLock::new(Args::parse);
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "vortex",
+    author = "Marley Reeves",
+    version,
+    about = "A file transfer tool written in Rust",
+    long_about = None,
+)]
+pub struct Args {
+    /// Mode
+    #[command(subcommand)]
+    pub cmd: Commands,
+
+    /// TCP port for file transfer
+    #[arg(short, long, global = true, default_value_t = 7070)]
+    pub port: u16,
+}
+
+#[derive(Debug, Clone)]
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Send a file
+    Send {
+        /// Path of the file to send
+        path: PathBuf,
+    },
+
+    /// Receive a file
+    Receive {
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use vortex::args::ARGS;
+use vortex::args::Commands;
+
+fn main() {
+    stderrlog::new().module(module_path!()).init().expect("failed to initialise logger");
+
+    match ARGS.cmd {
+        Commands::Send { path: _ } => send(),
+        Commands::Receive { } => receive(),
+    }
+}
